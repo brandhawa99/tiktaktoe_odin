fd87118d4f86148e00f88aeed0160bef187f80f8
Baltej Randhawa <baltej.randhawa0@gmail.com>
2021-12-10 11:07:32 -0800

    changed js

--- script.js
+++ script.js
@@ -1,59 +1,42 @@
+const mkPlayer = (name,sign)=>{
+    this.name = name ;
+    this.sign = sign; 
 
-
-const GameBoard = (function(){
-    const gameBoard = ["","","","","","","","",""];
-    
-    //accept vales to add to gameboard 
-    //paramters  index : which index of gameboard  
-    //paramater  shape: what the players move is (x/o);
-    function acceptValue(index, shape){
-        if(gameBoard[index] !== ""){
-            console.log("THERE IS ALREADY A PIECE THERE");
-            return;
-        }
-        gameBoard[index] = shape;
-        _render();
-    }
-    function resetBoard(){
-        for(let i = 0 ; i<gameBoard.length;i++){
-            gameBoard[i] = ""; 
-        }
-        _render();
-    }
-    
-    function _render(){
-        _buttons.forEach(button =>{
-            let value  = button.dataset.value
-            button.textContent = gameBoard[value];
-        })
-    }
-    function getBoardState(){
-        return gameBoard;
-    }
-    return{
-        acceptValue: addValue,
-        resetBoard: resetBoard,
-        getBoardState:getBoardState
-    }
-})();
-
-const player = (name,symbol) =>{
-    this.name = name ; 
-    this.symbol = symbol; 
-
-    const getSymbol = () =>{
-        return this.symbol; 
-    }
-
-    const getName = () =>{
+    function getSign(){
+        return this.sign
+    }  
+    function getName(){
         return this.name;
     }
-    return {getSymbol,getName}
+    
+    return{
+            getSign:getSign,
+            getName:getName
+        }
 }
 
-const game = (function(){
+const Game_Board = (() =>{
 
-    let player1 = player("baltej","x");
-    let player2 = player("layla","o");
-    const _buttons = document.querySelectorAll('#game_button');
+    const gameBoard = ["","","","","","","","",""];
+
+    const setValue = (index,sign) =>{
+        if(gameBoard[index] !== ""){
+            return;
+        }
+        gameBoard[index] = sign;
+
+    }
+
+    const getValue = (index) =>{
+        return gameBoard[index]
+    }
+    const resetBoard = () =>{
+        for(let i =0 ; i<gameBoard.length;i++){
+            gameBoard[i] = ""; 
+        }
+    }
+    return {
+        getValue,setValue,resetBoard
+    }
+
 })();
